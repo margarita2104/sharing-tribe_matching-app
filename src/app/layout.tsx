2fc0d9a0ac0f0137805e22974e36cebbddd589c3
Emir Murati <emirmurati@outlook.it>
2024-10-23 16:15:04 +0200

    google and linkedin auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
 import "~/styles/globals.css";
-
 import { Inter } from "next/font/google";
 import { type Metadata } from "next";
 import Header from "./_components/Shared/Header";
 import Footer from "./_components/Shared/Footer";
-
-import { getServerAuthSession } from "~/server/auth";
-import Providers from "./components/SessionProvider";
 
 export const metadata: Metadata = {
   title: "Sharing Tribe",
@@ -19,8 +15,6 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const session = await getServerAuthSession();
-
   return (
     <html lang="en" className={inter.className}>
       <body>
