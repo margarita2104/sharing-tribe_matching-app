72c51ed51096e01bceeed776fc4f3f23dcb34025
Emir Murati <emirmurati@outlook.it>
2024-10-23 14:50:15 +0200

    adding logic auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,25 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
+import { getServerAuthSession } from "~/server/auth";
+import Providers from "./components/SessionProvider";
+
 export const metadata: Metadata = {
   title: "Sharing Tribe",
   description: "Connect with people who share your interests.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const session = await getServerAuthSession();
+
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <Providers session={session}>{children}</Providers>
+      </body>
     </html>
   );
 }
